175aa85ed63424ae3b7e2dd7c4f3654c7c5f766a
ShwetaDoiphode <s.doiphode95@gmail.com>
2021-11-22 10:25:14 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      zip: ['', [Validators.required]],
+    });
+  }
+
+  onlyNumber(event): boolean {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      console.log('charcode not allow' + charCode);
+      return false;
+    }
+    return true;
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
