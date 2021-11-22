166b032ddf472aa62490fb551de53ed40f9f1a15
ShwetaDoiphode <s.doiphode95@gmail.com>
2021-11-22 14:04:50 +0530

    1 file modified

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   APIkey = '5a4b2d457ecbef9eb2a71e480b947604';
-  url = 'https://openweathermap.org/api';
+  url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
   constructor(private http: HttpClient) {}
 
-  getzipcode(zip) {
-    return this.http.get(this.url+ this.APIkey);
+  getWeather(zip: string): Observable<any> {
+    return this.http.get(this.url + zip + this.APIkey).pipe(
+      catchError((err) => {
+        if (err.status === 404) {
+          console.log(`City ${zip} not found`);
+          return EMPTY;
+        }
+      })
+    );
   }
 }
