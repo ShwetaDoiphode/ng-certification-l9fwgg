1467daae53fac403b1565f1ec9731ac6b7a9889d
ShwetaDoiphode <s.doiphode95@gmail.com>
2021-11-24 15:32:33 +0530

    1 file modified

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -12,38 +12,7 @@ import { ForecastService } from '../services/forecast.service';
   styleUrls: ['./forecast.component.css'],
 })
 export class ForecastComponent implements OnInit {
-  // @Input() zipcode: any;
-  // ZipCode: any;
-  // forecast: any = [];
-
-  // constructor(
-  //   private route: ActivatedRoute,
-  //   private apiforecast: ForecastService
-  // ) {}
-  // ngOnInit() {
-  //   console.log(this.zipcode);
-  //   const routeParams = this.route.snapshot.paramMap;
-  //   const zipCodeFromRoute = Number(routeParams.get('zipcode'));
-  //   console.log('Zip Code passed', zipCodeFromRoute);
-  //   this.ZipCode = zipCodeFromRoute;
-  //   this.getForecat(this.ZipCode);
-  // }
-
-  // getForecat(zipcode) {
-  //   this.apiforecast
-  //     .getFiveDaysForecast(zipcode)
-  //     .pipe(pluck('list'))
-  //     .subscribe((data) => {
-  //       this.furturforcast(data);
-  //     });
-  // }
-
-  // furturforcast(data: any) {
-  //   for (let i = 0; i < data.length; i = i + 8) {
-  //     this.forecast.push(data[i]);
-  //   }
-  //   console.log(this.forecast);
-  // }
+  
 
   @Input()
   locationData: CurrentWeather[] = [];
